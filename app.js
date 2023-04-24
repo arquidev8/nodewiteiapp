6b9bc1a3186c611317eb267a248a256e7a37778c
Hector <hectorcreatives08@gmail.com>
2023-04-24 16:50:32 -0400

    3

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const request = require('request');
+const path = require('path');
+
 const app = express();
 
+// Especificar el motor de plantillas
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/propiedades', (req, res) => {
   const url = 'https://witei.com/api/v1/houses/';
   const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
 
@@ -14,30 +23,28 @@ app.get('/', (req, res) => {
       res.status(500).send('Error interno del servidor');
     } else {
       const data = JSON.parse(body);
-      const propiedades_disponibles = data.results.filter(prop => prop.status === 'disponible');
-      res.render('propiedades', {propiedades: propiedades_disponibles});
+      res.render('propiedades', {propiedades: data.results});
     }
   });
 });
 
 app.get('/propiedad/:prop_id', (req, res) => {
-    const prop_id = req.params.prop_id;
-    const url = `https://witei.com/api/v1/houses/?identifier=${prop_id}`;
-    const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
-  
-    request({url, headers}, (error, response, body) => {
-      if (error) {
-        res.status(500).send('Error interno del servidor');
+  const prop_id = req.params.prop_id;
+  const url = `https://witei.com/api/v1/houses/?identifier=${prop_id}`;
+  const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
+
+  request({url, headers}, (error, response, body) => {
+    if (error) {
+      res.status(500).send('Error interno del servidor');
+    } else {
+      const data = JSON.parse(body);
+      if (data.count > 0 && data.results[0].identifier === prop_id) {
+        res.render('propiedad_detalle', {detalle: data.results[0]});
       } else {
-        const data = JSON.parse(body);
-        if (data.count > 0 && data.results[0].identifier === prop_id) {
-          res.render('propiedad_detalle', {detalle: data.results});
-        } else {
-          res.status(404).send('Propiedad no encontrada o no disponible');
-        }
+        res.status(404).send('Propiedad no encontrada o no disponible');
       }
-    });
+    }
   });
-app.listen(5000, () => {
-  console.log('La aplicación está funcionando en el puerto 5000');
 });
+
+module.exports = app;
