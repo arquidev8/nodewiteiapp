6e5be83c8e638878475534aad4e1e4aa8080ebff
Hector Hernandez <98498528+arquidev8@users.noreply.github.com>
2023-04-25 07:37:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,98 +1,44 @@
-// const express = require('express');
-// const request = require('request');
-// const app = express();
-
-// app.set('view engine', 'ejs');
-
-
-// app.get('/', (req, res) => {
-//   const url = 'https://witei.com/api/v1/houses/';
-//   const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
-
-//   request({url, headers}, (error, response, body) => {
-//     if (error) {
-//       res.status(500).send('Error interno del servidor');
-//     } else {
-//       const data = JSON.parse(body);
-//       const propiedades_disponibles = data.results.filter(prop => prop.status === 'disponible');
-//       res.render('propiedades', {propiedades: propiedades_disponibles});
-//     }
-//   });
-// });
-
-// app.get('/propiedad/:prop_id', (req, res) => {
-//     const prop_id = req.params.prop_id;
-//     const url = `https://witei.com/api/v1/houses/?identifier=${prop_id}`;
-//     const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
-  
-//     request({url, headers}, (error, response, body) => {
-//       if (error) {
-//         res.status(500).send('Error interno del servidor');
-//       } else {
-//         const data = JSON.parse(body);
-//         if (data.count > 0 && data.results[0].identifier === prop_id) {
-//           res.render('propiedad_detalle', {detalle: data.results});
-//         } else {
-//           res.status(404).send('Propiedad no encontrada o no disponible');
-//         }
-//       }
-//     });
-//   });
-// app.listen(5000, () => {
-//   console.log('La aplicación está funcionando en el puerto 5000');
-// });
-
 const express = require('express');
 const request = require('request');
-const path = require('path');
-
 const app = express();
 
-// Especificar el motor de plantillas
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/propiedades', (req, res) => {
   const url = 'https://witei.com/api/v1/houses/';
-  const headers = {'Authorization': `Bearer ${process.env.WITEI_API_KEY}`};
+  const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
 
   request({url, headers}, (error, response, body) => {
     if (error) {
       res.status(500).send('Error interno del servidor');
     } else {
       const data = JSON.parse(body);
-      res.render('propiedades', {propiedades: data.results});
+      const propiedades_disponibles = data.results.filter(prop => prop.status === 'disponible');
+      res.render('propiedades', {propiedades: propiedades_disponibles});
     }
   });
 });
 
 app.get('/propiedad/:prop_id', (req, res) => {
-  const prop_id = req.params.prop_id;
-  const url = `https://witei.com/api/v1/houses/?identifier=${prop_id}`;
-  const headers = {'Authorization': `Bearer ${process.env.WITEI_API_KEY}`};
-
-  request({url, headers}, (error, response, body) => {
-    if (error) {
-      res.status(500).send('Error interno del servidor');
-    } else {
-      const data = JSON.parse(body);
-      if (data.count > 0 && data.results[0].identifier === prop_id) {
-        res.render('propiedad_detalle', {detalle: data.results[0]});
+    const prop_id = req.params.prop_id;
+    const url = `https://witei.com/api/v1/houses/?identifier=${prop_id}`;
+    const headers = {'Authorization': 'Bearer 7a54f9633cf443d988c0c49e2b77989b'};
+  
+    request({url, headers}, (error, response, body) => {
+      if (error) {
+        res.status(500).send('Error interno del servidor');
       } else {
-        res.status(404).send('Propiedad no encontrada o no disponible');
+        const data = JSON.parse(body);
+        if (data.count > 0 && data.results[0].identifier === prop_id) {
+          res.render('propiedad_detalle', {detalle: data.results});
+        } else {
+          res.status(404).send('Propiedad no encontrada o no disponible');
+        }
       }
-    }
+    });
   });
+app.listen(5000, () => {
+  console.log('La aplicación está funcionando en el puerto 5000');
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Servidor iniciado en el puerto ${port}`);
-});
